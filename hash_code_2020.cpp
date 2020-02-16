4d62aa13f163229a55902c74d2611b76eccce7d7
HALOGENMAN <Shayak.malakar.159@gmail.com>
2020-02-16 20:31:36 +0530

    documentation

--- hash_code_2020.cpp
+++ hash_code_2020.cpp
@@ -29,17 +29,17 @@ int main(){
                sum-=a[i];
             }
             else{
-                location.push_back(i);
+                location.push_back(i); //agar sum max se chota hh to push krr dega ,push inthe sence push at top
             }
             
         }
-        location2.insert(pair<int,vector<int>>(sum,location));
+        location2.insert(pair<int,vector<int>>(sum,location)); // total sum push krne k bad uske sath elements ka vector bhe push krdega
     }
-    map<int,vector<int>>::iterator p = location2.end(); 
-    vector<int>::iterator k;
+    map<int,vector<int>>::iterator p = location2.end(); //vector inside map
+    vector<int>::iterator k; 
     p--;
-    cout<<p->second.size()<<endl;
-    //cout<<p->first<<endl;
+    cout<<p->second.size()<<endl; // gives tho elements length
+    //cout<<p->first<<endl; //this one is for printing sum
     for(int i= p->second.size()-1;i>=0;i--){
         cout<< p->second[i]<<" ";
     }
